fa9be3a780cab1ebb67f20f29613218da2ffbc1d
Rodolphe Célestin <rodolphecelestin@gmail.com>
2025-06-01 12:56:41 +0200

    Initial Commit

---
+++ ListGuestAPP/ListGuestAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListGuestAPPApp.swift
+//  ListGuestAPP
+//
+//  Created by Rodolphe Celestin on 2025-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ListGuestAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListGuestAPPTests/ListGuestAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListGuestAPPTests.swift
+//  ListGuestAPPTests
+//
+//  Created by Rodolphe Celestin on 2025-06-01.
+//
+
+import Testing
+@testable import ListGuestAPP
+
+struct ListGuestAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
